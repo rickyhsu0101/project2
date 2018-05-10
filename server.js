fd377561a7519b22c12781cb0da3fe83bb59f4bf
Ricky Hsu <rickyhsu0101@gmail.com>
2018-05-10 15:43:53 -0700

    Setup basic server with config

--- server.js
+++ server.js
@@ -3,6 +3,29 @@ var ejs = require("ejs");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 var expressValidator = require("express-validator");
+var dotenv = require("dotenv");
+var path = require("path");
 
+dotenv.config();
+
+
+var PORT = 8080;
 
 var app = express();
+
+
+
+//view engine
+app.set("views", )
+app.set("view engine", "ejs");
+
+//middleware
+
+app.use(bodyParser.json());
+app.use(bodyParser.bodyParser({extended: true}));
+app.use(expressValidator());
+
+
+app.listen(PORT, function(){
+    console.log("listening on " + PORT);
+});
