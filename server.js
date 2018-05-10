003e672c510d85768c6e32a98bfebb6b791c3c3a
Ricky Hsu <rickyhsu0101@gmail.com>
2018-05-10 14:57:21 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var ejs = require("ejs");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var expressValidator = require("express-validator");
+
+
+var app = express();
