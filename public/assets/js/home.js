2d5144f8e9332a398e7d9333ec9b93166178a475
Ricky Hsu <rickyhsu0101@gmail.com>
2018-05-10 18:40:29 -0700

    Added header ejs, routes and css loader

---
+++ public/assets/js/home.js
@@ -0,0 +1,6 @@
+import {headerInit} from  './header.js';
+
+
+$(document),ready(function(){
+    headerInit();
+});
--- public/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-import {output} from  './another.js';
-
-var hello = (name) =>{
-    console.log(name);
-};
-output("whywhywhy");
-hello("hello");
