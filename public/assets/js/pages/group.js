06f281366a976ca7f7de4edb61c944b52a338128
Trevor Johnson <johnsontrevor55@gmail.com>
2018-05-19 12:46:54 -0700

    styling group and profile page

--- public/assets/js/pages/group.js
+++ public/assets/js/pages/group.js
@@ -5,44 +5,71 @@ import '../../css/group.css';
 
 $(document).ready(function() {
   headerInit();
-  let membersQuery = "/api/group/" + $("#membersDiv").data("group").trim() + "/members";
+
+  $(document).ready(function() {
+    $('.collapsible').collapsible();
+  });
+
+  let membersQuery =
+    '/api/group/' +
+    $('#membersDiv')
+      .data('group')
+      .trim() +
+    '/members';
   $.ajax({
-    method: "GET",
+    method: 'GET',
     url: membersQuery
-  }).done(function(response){
-    let power = $("#membersDiv").data("power").trim();
-    let currentUserId = $("#membersDiv").data("userid").trim();
-    if(!response.error){
+  }).done(function(response) {
+    let power = $('#membersDiv')
+      .data('power')
+      .trim();
+    let currentUserId = $('#membersDiv')
+      .data('userid')
+      .trim();
+    if (!response.error) {
       console.log(response);
       console.log(response.data);
       var data = response.data;
       for (var i = 0; i < data[0].length; i++) {
         let memberHTML = $("<div class = 'member row'></div>");
-        memberHTML.append("<div class = 'col s8'><a class= 'memberInfo' href = " +
-        "'/profile/" + data[0][i].userId +
-        "' data-id='" + data[0][i].userId + "'><h4>" +
-        data[0][i].username + "</h4></a></div>");
-        if (power == "admin") {
+        memberHTML.append(
+          "<div class = 'col s8'><a class= 'memberInfo' href = " +
+            "'/profile/" +
+            data[0][i].userId +
+            "' data-id='" +
+            data[0][i].userId +
+            "'><h4>" +
+            data[0][i].username +
+            '</h4></a></div>'
+        );
+        if (power == 'admin') {
           if (data[0][i].userId != currentUserId) {
-            memberHTML.append("<div class = 'col s4'><a class = 'wave-effect waves-light btn' href = '#'>" + "Remove" + "</a></div>");
+            memberHTML.append(
+              "<div class = 'col s4'><a class = 'wave-effect waves-light btn' href = '#'>" +
+                'Remove' +
+                '</a></div>'
+            );
           }
         }
-        $("#membersDiv").append(memberHTML);
+        $('#membersDiv').append(memberHTML);
       }
     }
-    
   });
-  $("#joinGroup").on("click", function(e){
+  $('#joinGroup').on('click', function(e) {
     e.preventDefault();
-    var userId = $(this).data("user").trim();
-    var groupId = $(this).data("group").trim();
-    var query = "/api/group/" + groupId + "/join/" + userId;
+    var userId = $(this)
+      .data('user')
+      .trim();
+    var groupId = $(this)
+      .data('group')
+      .trim();
+    var query = '/api/group/' + groupId + '/join/' + userId;
     $.ajax({
-      method: "GET",
+      method: 'GET',
       url: query
-    }).done(function(data){
-      if(!data.error){
-        $("#joinGroupDiv").remove();
+    }).done(function(data) {
+      if (!data.error) {
+        $('#joinGroupDiv').remove();
       }
     });
   });
--- public/assets/js/pages/groups.js
+++ public/assets/js/pages/groups.js
@@ -1,4 +1,5 @@
 import { headerInit } from '../general/header.js';
+import '../../css/groups.css';
 import '../../css/style.css';
 
 $(document).ready(function() {
